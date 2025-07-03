0e92701d9f58ec3aaa0326b5d378dce7ae6bc30c
bayuraditya <bayuraditya35@gmail.com>
2025-07-03 09:26:18 +0800

    adding dropdown navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -17,30 +17,17 @@
                         <li><a href="{{ url('/#hero') }}" class="active">Home</a></li>
                         <li><a href="{{ url('/#why-choose-us') }} ">About</a></li>
                         <li><a href="{{ url('/#services')}}">Services</a></li>
-                        <li><a href="{{ url('/#project') }}">Project</a></li>
+                        <li class="dropdown">
+                          <a href="{{ url('/#project') }}"><span>Project</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                          <ul>
+                            <li><a href="{{ url('/project') }}">Semua Project</a></li>
+                          </ul>
+                        </li> 
                         <!-- <li><a href="{{ url('/#portfolio') }}">Portfolio</a></li> -->
                         <li><a href="{{ url('/#galleries') }}">Galleries</a></li>
                         <!-- <li><a href="#pricing">Pricing</a></li> -->
                         <li><a href="{{ url('/#team') }}">Team</a></li>
-                        <!-- <li class="dropdown">
-              <a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-              <ul>
-                <li><a href="#">Dropdown 1</a></li>
-                <li class="dropdown">
-                  <a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-                  <ul>
-                    <li><a href="#">Deep Dropdown 1</a></li>
-                    <li><a href="#">Deep Dropdown 2</a></li>
-                    <li><a href="#">Deep Dropdown 3</a></li>
-                    <li><a href="#">Deep Dropdown 4</a></li>
-                    <li><a href="#">Deep Dropdown 5</a></li>
-                  </ul>
-                </li>
-                <li><a href="#">Dropdown 2</a></li>
-                <li><a href="#">Dropdown 3</a></li>
-                <li><a href="#">Dropdown 4</a></li>
-              </ul>
-            </li> -->
+                       
                         <li><a href="{{ url('/#contact') }}">Contact</a></li>
                     </ul>
                     <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
